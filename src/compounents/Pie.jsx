04141aa184079c483d272988504cb19ee0299437
ibrahimsaber1 <ibrahimsaber622@gmail.com>
2025-11-03 19:29:40 +0200

    add: piechart data

--- src/compounents/Pie.jsx
+++ src/compounents/Pie.jsx
@@ -1,3 +1,14 @@
+import React from "react";
+import { Pie } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  Tooltip,
+  Legend,
+  ArcElement
+} from "chart.js";
+
+
+
 export const PieChart = () => {
 
     return <> hi from the pie compounent</> ;
--- src/fakeData.jsx
+++ src/fakeData.jsx
@@ -31,12 +31,27 @@ export const BarData = {
             backgroundColor : "#f19595ff",
             borderColor: "rgba(255, 255, 255, 1)",
             borderWidth: 4,
-        },
-        // {
-        //     label : "ibrahims steps",
-        //     data:[1100 ,2200,2300,3500, 11500, 5000],
-        //     borderColor : "rgba(86, 84, 158, 1)"
-        // }
+        }
     ],
 };
+
+
+export const PieData = {
+    labels: ["Facebook", "Instagram", "Twitter", "Youtube", "Linkedin"],
+    datasets: [
+        {
+            label: "Time Spent",
+            data: [120,60,30,90,45],
+            backgroundColor : [
+                                "rgba(241, 149, 149, 1)",
+                                "rgba(77, 151, 58, 1)",
+                                "rgba(26, 30, 51, 1)",
+                                "rgba(136, 135, 99, 1)",
+                                "rgba(202, 77, 171, 1)",
+                                ],
+            hoverOffset: 4,
+        }
+    ],
+};
+
 
